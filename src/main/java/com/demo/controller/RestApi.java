23f4500c1bca9fc95b466cdea08519ed0c9081c3
German Gomez <gomez9814@hotmail.com>
2024-08-28 20:10:03 -0500

    R001: add refactor phase 1

--- src/main/java/com/demo/controller/RestApi.java
+++ src/main/java/com/demo/controller/RestApi.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 
 import com.demo.service.Services;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class RestApi {
+
     private final Services services;
-    @GetMapping("api/v1/{id}")
+
+    @GetMapping("api/v1/{name}")
     public String getName(@PathVariable String name) {
         return services.getName(name);
     }
